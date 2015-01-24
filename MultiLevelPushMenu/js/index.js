d34dd827d7b9c2df94a325c29e312065d0723580
shawniscool <xiangpan@Jun-Zhengs-MacBook-Pro.local>
2015-01-24 18:03:27 -0500

    for terry

---
+++ MultiLevelPushMenu/js/index.js
@@ -0,0 +1,63 @@
+var swarthmore = ["Ancient History",
+"Anthropology",
+"Arabic",
+"Art",
+"Art History",
+"Asian Studies",
+"Astronomy",
+"Biochemistry",
+"Biology",
+"Black Studies",
+"Chemistry",
+"Chinese",
+"Classics",
+"Cognitive Science",
+"Comparative Literature",
+"Computer Science",
+"Dance",
+"Economics",
+"Educational Studies",
+"Engineering",
+"English Literature",
+"Environmental Studies",
+"Film & Media Studies",
+"French & Francophone Studies",
+"Gender & Sexuality Studies",
+"German Studies",
+"Greek",
+"History",
+"Interpretation Theory",
+"Islamic Studies",
+"Japanese",
+"Latin",
+"Latin American Studies",
+"Linguistics",
+"Math and Stats",
+"Medieval Studies",
+"Modern Languages & Literatures",
+"Music",
+"Peace & Conflict Studies",
+"Philosophy",
+"Physics",
+"Political Science",
+"Psychology",
+"Public Policy",
+"Religion",
+"Russian",
+"Sociology",
+"Spanish",
+"Statistics",
+"Theater"]
+
+$(function(){
+	$('#swarthmore' > 'ul').append("<li><a href='#'> apple </a></li>");
+	// $('#heading').append("abc");
+	alert("worked");
+
+	// Parse.initialize("puHovjluHz95PXkN2Wj5xAwZ6pEB3KQfw5k3ZbGt", "BYcb3EbT8VAt08L4wdO1SNvBFxmiP02CimiiZz04");
+	// var Book = Parse.Object.extend("Book");
+	// var query = new Parse.Query("Book");
+	// query.equalTo("department","ANTH");
+	// // $(.college)
+
+});
